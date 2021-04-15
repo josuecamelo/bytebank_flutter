c26ed799f28c3aec0978b8b5635a1d4c0173fbeb
Josué Camelo <josueprg@gmail.com>
2021-04-15 17:52:57 -0300

    Alterações Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,92 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      body: ListaTransferencias(),
+  runApp(BytebankApp());
+}
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
+      ),
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  final TextEditingController _controladorCampoNumeroConta =
+  TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: Text('Transferências'),
+        title: Text('Criando Transferências'),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
-    ),
-  ));
+      body: Column(children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            controller: _controladorCampoNumeroConta,
+            style: TextStyle(fontSize: 24.0),
+            decoration: InputDecoration(
+              labelText: 'Número da Conta',
+              hintText: '0000',
+            ),
+            keyboardType: TextInputType.number,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            controller: _controladorCampoValor,
+            style: TextStyle(fontSize: 24.0),
+            decoration: InputDecoration(
+              icon: Icon(Icons.monetization_on),
+              labelText: 'Valor',
+              hintText: '0.00',
+            ),
+            keyboardType: TextInputType.number,
+          ),
+        ),
+        RaisedButton(
+          onPressed: () {
+            debugPrint('clicou no confirmar');
+            final int numeroConta = int.tryParse(_controladorCampoNumeroConta.text);
+            final double valor = double.tryParse(_controladorCampoValor.text);
+
+            //if(numeroConta!= null && valor != null){
+              final transferenciaCriada = Transferencia(valor, numeroConta);
+              debugPrint('$transferenciaCriada');
+            //}
+          },
+          child: Text('Confirmar'),
+        )
+      ]),
+    );
+  }
 }
 
 class ListaTransferencias extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ItemTransferencia(Transferencia(100.00, '1000')),
-        ItemTransferencia(Transferencia(200.00, '2000')),
-        ItemTransferencia(Transferencia(300.00, '3000')),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
+      ),
+      body: Column(
+        children: [
+          // ItemTransferencia(Transferencia(100.00, '1000')),
+          // ItemTransferencia(Transferencia(200.00, '2000')),
+          // ItemTransferencia(Transferencia(300.00, '3000')),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
@@ -37,16 +100,21 @@ class ItemTransferencia extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
         child: ListTile(
-      leading: Icon(Icons.monetization_on),
-      title: Text(_transferencia.valor.toString()),
-      subtitle: Text(_transferencia.numeroConta),
-    ));
+          leading: Icon(Icons.monetization_on),
+          title: Text(_transferencia.valor.toString()),
+          subtitle: Text(_transferencia.numeroConta.toString()),
+        ));
   }
 }
 
 class Transferencia {
   final double valor;
-  final String numeroConta;
+  final int numeroConta;
 
   Transferencia(this.valor, this.numeroConta);
+
+  @override
+  String toString() {
+    return 'Transferencia{valor: $valor, numeroConta: $numeroConta}';
+  }
 }
